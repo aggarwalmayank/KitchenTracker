b22d3b06aedfceb919a8c4c1d66f23779cc6507b
aggarwalmayank <QxiW4634+>
2019-11-13 21:38:52 +0530

    code uploaded

---
+++ app/src/main/java/com/appsaga/kitchentracker/MainActivity.java
@@ -0,0 +1,19 @@
+package com.appsaga.kitchentracker;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+    TextView t1,t2;
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
